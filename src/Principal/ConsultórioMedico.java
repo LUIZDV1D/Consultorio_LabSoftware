e73c6402635a948151bb973f2ca66dfa9bdeb262
Pedro Alcantara <Pedro Alcantara@DESKTOP-RBALN3C>
2019-02-20 13:28:04 -0300

    Commit inicial

---
+++ src/Principal/ConsultórioMedico.java
@@ -0,0 +1,11 @@
+package Principal;
+public class ConsultórioMedico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
